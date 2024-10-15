77f7a805098d8bc00846a634384614f3fe7cf181
Jinho <baejin922@gmail.com>
2024-10-15 16:54:01 +0900

    #6.7 Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,21 +1,52 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList() {
+/* function ToDoList() {
     const [toDo, setTodo] = useState("");
+    const [toDoError, setToDoError] = useState("");
     const onChange = (event:React.FormEvent<HTMLInputElement>) => {
         const {
             currentTarget: { value },
         } = event;
+        setToDoError("");
         setTodo(value);
     };
     const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log(toDo);
+        if (toDo.length < 10) {
+            return setToDoError("To do shuould be longer");
+        }
+        console.log("submit");
     };
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+                <button>Add</button>
+                {toDoError !== "" ? toDoError : null}
+            </form>
+        </div>
+    );
+} */
+
+function ToDoList() {
+    const { register, handleSubmit, formState } = useForm();
+    const onValid = (data: any) => {
+        console.log(data);
+    }
+    console.log(formState.errors);
+    return (
+        <div>
+            <form
+                style={{ display: "flex", flexDirection: "column" }}
+                onSubmit={handleSubmit(onValid)}
+            >
+                <input {...register("email", { required: true })} placeholder="email" />
+                <input {...register("firstName", { required: true })} placeholder="First Name" />
+                <input {...register("lastName", { required: true })} placeholder="Last Name" />
+                <input {...register("username", { required: true, minLength: 10 })} placeholder="Username" />
+                <input {...register("password", { required: true, minLength: 5 })} placeholder="Password" />
+                <input {...register("password1", { required: "Password is required", minLength: { value: 5, message: "Your password is too short."} })} placeholder="Password1" />
                 <button>Add</button>
             </form>
         </div>
